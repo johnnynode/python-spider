df00e1ae43478f81bd55181ed5c36d2873fcd317
johnnynode <johnnynode@gmail.com>
2019-10-21 06:21:35 +0800

    update: test scrapy wenku multi request

--- basic/contents/code/wenku_search/wenku_search/spiders/wenku.py
+++ basic/contents/code/wenku_search/wenku_search/spiders/wenku.py
@@ -5,10 +5,12 @@ import scrapy
 class WenkuSpider(scrapy.Spider):
     name = 'wenku'
     allowed_domains = ['wenku.baidu.com']
-    start_urls = ['https://wenku.baidu.com/search?word=python&pn=0']
+    start_urls = ['https://wenku.baidu.com/search?word=python&pn=0','https://wenku.baidu.com/search?word=python&pn=10','https://wenku.baidu.com/search?word=python&pn=20']
 
     def parse(self, response):
         dllist = response.selector.xpath("//dl")
         #print(len(dllist))
         for dd in dllist:
             print(dd.xpath("./dt/p/a/@title").extract_first())
+
+        print("="*70) #输出一条每个请求后分割线
