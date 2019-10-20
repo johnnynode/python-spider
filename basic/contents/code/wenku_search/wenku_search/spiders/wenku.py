511056f4ce546ab35b96dee4154a98d02d9ee306
johnnynode <johnnynode@gmail.com>
2019-10-21 06:47:26 +0800

    update: test scrapy wenku multi request in a loop

--- basic/contents/code/wenku_search/wenku_search/spiders/wenku.py
+++ basic/contents/code/wenku_search/wenku_search/spiders/wenku.py
@@ -5,7 +5,8 @@ import scrapy
 class WenkuSpider(scrapy.Spider):
     name = 'wenku'
     allowed_domains = ['wenku.baidu.com']
-    start_urls = ['https://wenku.baidu.com/search?word=python&pn=0','https://wenku.baidu.com/search?word=python&pn=10','https://wenku.baidu.com/search?word=python&pn=20']
+    start_urls = ['https://wenku.baidu.com/search?word=python&pn=0']
+    p = 0
 
     def parse(self, response):
         dllist = response.selector.xpath("//dl")
@@ -13,4 +14,12 @@ class WenkuSpider(scrapy.Spider):
         for dd in dllist:
             print(dd.xpath("./dt/p/a/@title").extract_first())
 
-        print("="*70) #输出一条每个请求后分割线
+        print("="*70)
+
+        self.p += 1
+        if self.p < 10:
+            next_url = 'https://wenku.baidu.com/search?word=python&pn=' + str(self.p * 10)
+            yield scrapy.Request(url=next_url,callback=self.parse)
+
+            # url = response.urljoin(next_url) # 构建绝对url地址（这里可省略）
+            # yield scrapy.Request(url=url,callback=self.parse)
